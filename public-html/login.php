6980f3f5663e434f334ca58307f2962527519c73
angeles-fernandez <angelesfernandez2810@gmail.com>
2023-01-19 19:57:53 +0100

    Deploying

---
+++ public-html/login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Formulario login</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="cuadro">
+    <h1>Acceso a la pagina</h1>
+    <hr>
+    <p><b> Introduzca usuario y contraseña </b></p>
+    <br>
+
+    <form method="POST" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" name="login">
+
+        <div class="usuario">
+            <label>Usuario:
+                <input type="text" name="user" pattern="[a-zA-Z0-9]+" required />
+            </label>
+        </div>
+        <br>
+        <div class="contraseña">
+            <label>Contraseña:
+                <input type="password" name="pass" required />
+            </label>
+        </div>
+        <button type="submit" name="login" value="login">Log In</button>
+    </form>
+
+    <?php
+    session_start();
+    if (isset($_POST['user']) && isset($_POST['pass'])) {
+        if ($_POST['user'] == "foc" && $_POST['pass'] == "Fdwes!22") {
+            $_SESSION['tlf'] = "";
+            $_SESSION['email'] = "";
+            header("Location: http://localhost/tarea4/sesion.php");
+            exit;
+        } else {
+            echo "Credenciales incorrectas";
+        }
+    }
+    ?>
+</body>
+</html>
---
+++ public-html/sesion.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Formulario sesion</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <?php
+    session_start();
+    if (!isset($_SESSION['tlf']) || !isset($_SESSION['email'])) {
+        header('Location: login.php');
+        exit;
+    }
+
+    if (isset($_POST['grabar'])) {
+        if (isset($_POST['telefono']) && isset($_POST['email'])) {
+            $_SESSION['tlf'] = $_POST['telefono'];
+            $_SESSION['email'] = $_POST['email'];
+            header("Location: http://localhost/tarea4/sesion.php");
+            exit;
+        }
+    } elseif (isset($_POST['borrar'])) {
+        $_SESSION['tlf'] = "";
+        $_SESSION['email'] = "";    
+        setcookie('horario', '', time() - 3600);
+        header("Location: http://localhost/tarea4/sesion.php");
+        exit;
+    } elseif (isset($_POST['grabar_horario'])) {
+        if (isset($_POST['horario'])) {
+            setcookie('horario', $_POST['horario'], time() + 3600);
+            header("Location: http://localhost/tarea4/sesion.php");
+            exit;
+        }
+    } elseif (isset($_POST['borrar_horario'])) {
+    
+    }
+    ?>
+    <form method="POST" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" name="sesion">
+
+        <h1>Acceso concedido</h1>
+        <hr>
+        <p><b> Introduzca telefono y email </b></p>
+        <br>
+
+        <div class="telefono">
+            <label>Teléfono:
+                <input type="text" id="telefono" name="telefono" value="<?php if (isset($_SESSION['tlf'])) {
+                                                                            echo $_SESSION['tlf'];
+                                                                        } ?>" required />
+            </label>
+        </div>
+        <br>
+        <div class="email">
+            <label>Email:
+                <input type="text" id="email" name="email" value="<?php if (isset($_SESSION['email'])) {
+                                                                        echo $_SESSION['email'];
+                                                                    } ?>" required />
+            </label>
+        </div>
+        <br>
+        <br>
+        <button type="submit" name="grabar" value="Grabar">Grabar</button>
+        <button type="submit" name="borrar" value="Borrar">Borrar</button>
+        <br><br><br>
+    </form>
+
+    <form method="POST" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" name="horario_form">
+        <select name="horario">
+            <option value="mañana" <?php if (isset($_COOKIE['horario']) && $_COOKIE['horario'] == 'mañana') {
+                                        echo 'selected';
+                                    } ?>>Mañana</option>
+            <option value="tarde" <?php if (isset($_COOKIE['horario']) && $_COOKIE['horario'] == 'tarde') {
+                                        echo 'selected';
+                                    } ?>>Tarde</option>
+            <option value="noche" <?php if (isset($_COOKIE['horario']) && $_COOKIE['horario'] == 'noche') {
+                                        echo 'selected';
+                                    } ?>>Noche</option>
+        </select>
+
+        <br><br>
+        <button type="submit" name="grabar_horario" value="Grabar Horario">Grabar Horario</button>
+        <br><br><br>
+    </form>
+</body>
+</html>
