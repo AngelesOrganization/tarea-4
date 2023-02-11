28291948dc8b28349e98e10efbaac20f5960c430
angeles-fernandez <angelesfernandez2810@gmail.com>
2023-02-11 18:14:13 +0100

    v2 pokemon

--- public-html/tarea9/pokemon.php
+++ public-html/tarea9/pokemon.php
@@ -1,5 +1,7 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,21 +9,27 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
+
 <body>
-<?php
+    <?php
 
-    $limit = 10;
-    $currentOffset = 0;
+    if(isset($_SESSION['offset'])){
+        $currentOffset = $_SESSION['offset'];
+    } else {
+        $currentOffset = 0;
+    }
 
-    function getPokemons($limit, $currentOffset) {
-        $url = "https://pokeapi.co/api/v2/pokemon?limit=$limit&offset=$currentOffset";
+    function getPokemons($currentOffset)
+    {
+        $url = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=$currentOffset";
         $respuesta = json_decode(file_get_contents($url), true);
         $tbody = "<tbody>";
+        $self = htmlentities($_SERVER['PHP_SELF']);
 
         foreach ($respuesta['results'] as $pokemon) {
             $tbody .= "<tr>";
             $tbody .= "<td>$pokemon[name]</td>";
-            $tbody .= "<td><button onclick='getPokemonDetail(\"$pokemon[url]\");'>Detalle</button></td>";
+            $tbody .= "<td><form method='GET' action='$self' name='detalle'> <button type='submit' name='detalle' value='$pokemon[url]'>Detalle</button> </form></td>";
             $tbody .= "</tr>";
         }
 
@@ -30,7 +38,8 @@
         return $tbody;
     }
 
-    function getPokemonDetail($url) {
+    function getPokemonDetail($url)
+    {
         $respuesta = json_decode(file_get_contents($url), true);
         $name = $respuesta['name'];
         $hp = $respuesta['stats'][0]['base_stat'];
@@ -55,68 +64,45 @@
         return $detail;
     }
 
-?>
+    ?>
+
     <body>
-    <div id="lista">
-        <input id="clickMe" type="button" value="previous" onclick="previous();" />
-        <input id="clickMe" type="button" value="next" onclick="next();" />
-        <label for='number-dd'><b>Limit</b></label>
-        <select onchange="newLimit(this.value)" id='number-dd' name='number'>
-            <option value=10>10</option>
-            <option value=20>20</option>
-            <option value=30>30</option>
-        </select>
-        <table id="pokemons">
-            <thead>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Detalle</th>
-                </tr>
-            </thead>
-            <?php
-                global $limit, $currentOffset; 
-                $tag = getPokemons($limit, $currentOffset);
+        <div id="lista">
+            <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
+                <input type="submit" name="next" value="Next">
+                <?php
+                if (isset($_POST['next'])) {
+                    if(isset($_SESSION['offset'])) {
+                        $_SESSION['offset'] = intval($_SESSION['offset']) + 20;
+                    } else {
+                        $_SESSION['offset'] = 20;
+                    }
+                }
+                ?>
+            </form>
+            <table id="pokemons">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Detalle</th>
+                    </tr>
+                </thead>
+                <?php
+                global $currentOffset;
+                $tag = getPokemons($currentOffset);
                 echo $tag;
-            ?>
-        </table>
-    </div>
-    <div id="detalle" hidden>
-        <div>
-            <table id="detail">
-                <tr>
-                    <th>Name</th>
-                    <td id="name"></td>
-                </tr>
-                <tr>
-                    <th>Image</th>
-                    <td><img id="pkimage" src="" alt="" width="200px" height="200px"></td>
-                </tr>
-                <tr>
-                    <th>HP</th>
-                    <td id="hp"></td>
-                </tr>
-                <tr>
-                    <th>Attack</th>
-                    <td id="attack"></td>
-                </tr>
-                <tr>
-                    <th>Defense</th>
-                    <td id="defense"></td>
-                </tr>
-                <tr>
-                    <th>SP-Attack</th>
-                    <td id="spattack"></td>
-                </tr>
-                <tr>
-                    <th>SP-Defense</th>
-                    <td id="spdefense"></td>
-                </tr>
-                <tr>
-                    <th>Speed</th>
-                    <td id="speed"></td>
-                </tr>
+                ?>
             </table>
         </div>
-    </div>
-</body>
+        <div id="detalle">
+            <div>
+                <?php
+                if (isset($_GET['detalle'])) {
+                    echo getPokemonDetail($_GET['detalle']);
+                }
+                ?>
+            </div>
+        </div>
+    </body>
+
 </html>
