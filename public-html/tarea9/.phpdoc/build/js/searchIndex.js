d7c173032a7e49791cf49208d47670f1d4d47c80
angeles-fernandez <angelesfernandez2810@gmail.com>
2023-02-19 16:25:32 +0100

    PHP DOC

---
+++ public-html/tarea9/.phpdoc/build/js/searchIndex.js
@@ -0,0 +1,19 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\getPokemons\u0028\u0029",
+            "name": "getPokemons",
+            "summary": "",
+            "url": "namespaces/default.html#function_getPokemons"
+        },                {
+            "fqsen": "\\getPokemonDetail\u0028\u0029",
+            "name": "getPokemonDetail",
+            "summary": "",
+            "url": "namespaces/default.html#function_getPokemonDetail"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
